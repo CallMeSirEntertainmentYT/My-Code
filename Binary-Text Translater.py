eb42fcf60e849f9f3d1896917533fa7a72a86a3f
CallMeSirEntertainment <callmesirentertainment@outlook.com>
2023-12-19 15:04:17 -0600

    Update 1.0

---
+++ Binary-Text Translater.py
@@ -0,0 +1,24 @@
+def string_to_binary(input_string):
+    return ''.join(format(ord(i), '08b') for i in input_string)
+
+def binary_to_string(input_binary):
+    binary_values = [input_binary[i:i+8] for i in range(0, len(input_binary), 8)]
+    return ''.join(chr(int(binary, 2)) for binary in binary_values)
+
+while True:
+    # Get user input
+    user_input = input("Enter a string to convert to binary, or binary to convert to a string (or 'n' to quit): ")
+
+    # Check if the user wants to quit
+    if user_input.lower() == 'n':
+        break
+
+    # Check if the input is binary
+    if set(user_input) == {'0', '1'}:
+        print("Input is binary. Converting to string...")
+        print("String: ", binary_to_string(user_input))
+    else:
+        print("Input is a string. Converting to binary...")
+        print("Binary: ", string_to_binary(user_input))
+
+print("Goodbye!")
