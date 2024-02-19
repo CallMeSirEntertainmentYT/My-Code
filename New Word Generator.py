08b85f3705418aedacfdbae208cffb54a9703a31
CallMeSirEntertainment <callmesirentertainment@outlook.com>
2024-02-19 16:36:46 -0600

    Update 1.0
    Generates a random word with 3 to 10 digits. Also generates a random definition for the word.

---
+++ New Word Generator.py
@@ -0,0 +1,70 @@
+import random
+import time
+import string
+vowels = string.ascii_lowercase[0:5]
+consonants = string.ascii_lowercase[5:]
+def choose_letter():
+  r = random.random()
+  if r < 0.5:
+    return random.choice(vowels)
+  else:
+    return random.choice(consonants)
+def make_word():
+  length = random.randint(3, 10)
+  word = ""
+  vowel_count = 0
+  for i in range(length):
+    letter = choose_letter()
+    word += letter
+    if letter in vowels:
+      vowel_count += 1
+  if vowel_count == 0:
+    word = word[:-1] + random.choice(vowels)
+  return word
+
+def make_definition(word):
+  definition = ""
+  length = len(word)
+  vowel_count = sum(letter in vowels for letter in word)
+  consonant_count = length - vowel_count
+  first_letter = word[0]
+  last_letter = word[-1]
+  if vowel_count > consonant_count:
+    if first_letter in vowels:
+      definition += "an "
+    else:
+      definition += "a "
+    if last_letter == "a":
+      definition += "type of cheese"
+    elif last_letter == "e":
+      definition += "musical instrument"
+    elif last_letter == "i":
+      definition += "rare disease"
+    elif last_letter == "o":
+      definition += "mythical creature"
+    elif last_letter == "u":
+      definition += "slang term for money"
+    else:
+      definition = "a random noun"
+  else:
+    definition += "to "
+    if last_letter == "a":
+      definition += "dance"
+    elif last_letter == "e":
+      definition += "sing"
+    elif last_letter == "i":
+      definition += "study"
+    elif last_letter == "o":
+      definition += "jump"
+    elif last_letter == "u":
+      definition += "run"
+    else:
+      definition = "a random verb"
+  return definition
+
+while True:
+  word = make_word()
+  print(word)
+  definition = make_definition(word)
+  print(definition)
+  time.sleep(5)
