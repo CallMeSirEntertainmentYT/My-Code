7ed57ed499c02efd3eaee3135d9b5809ff715f00
CallMeSirEntertainment <callmesirentertainment@outlook.com>
2024-03-22 20:45:23 -0500

    Update 1.8
    Added easter eggs :)

--- Guessing Game.py
+++ Guessing Game.py
@@ -1,4 +1,4 @@
-import random
+import random as rand
 import os
 import sys
 import time
@@ -10,8 +10,21 @@ if play_again_input.lower() == "y":
         play_again = True
 else:
     play_again = False
+    wait_time1 = rand.randint(1,4)
+    wait_time2 = rand.randint(1,4)
+    wait_time3 = rand.randint(1,4)
+    wait_time4 = rand.randint(1,4)
     print("\nWhy did you run this program then?")
-
+    time.sleep(wait_time1)
+    print("Are you serious?")
+    time.sleep(wait_time2)
+    print("You're just going to give up?")
+    time.sleep(wait_time3)
+    print("AT A GUESSING GAME?")
+    time.sleep(wait_time4)
+    print("Fine.")
+    time.sleep(2)
+    sys.exit
 while play_again:
     age = None
     while age is None:
@@ -48,9 +61,13 @@ while play_again:
     else:
         print("Invalid level of difficulty. Please choose again.")
         continue
+    special_numbers = [69, 420, 21]
+    special_number = rand.choice(special_numbers)
+    if number == special_number:
+        print("Haha, funny number.")
     print(f"\nI'm thinking of a number between 1 and {max_number}.")
     print(f"You have {tries} tries to guess the number.")
-    number = random.randint(1, max_number)
+    number = rand.randint(1, max_number)
     for i in range(tries):
         guess = int(input("\nEnter your guess: "))
 
@@ -65,11 +82,13 @@ while play_again:
         print(f"\nSorry, you ran out of tries. \nThe number was {number}.")
         if not safe_mode:
             print("\nDeleting the game as you failed to guess the number in ten tries.")
-            time.sleep(5)  # Wait for 5 seconds
+            time.sleep(3)
             os.remove(sys.argv[0])
             sys.exit()
     play_again_input = input("\nWould you like to play again? (y/n) ")
     if play_again_input.lower() == "n":
         play_again = False
         print("\nThank you for playing!")
+        time.sleep(3)
+        sys.exit
 
